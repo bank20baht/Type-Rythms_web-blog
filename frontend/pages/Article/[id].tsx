b7e07d785c5298ec8661b683875651e584a23bc7
Nattapong <banker.z412@gmail.com>
2023-04-19 17:22:15 +0700

    modify Article page to use getServerSideProps

--- frontend/pages/Article/[id].tsx
+++ frontend/pages/Article/[id].tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-export type ArticleData = {
+type ArticleData = {
   _id: string;
   title: string;
   content: string;
@@ -14,43 +14,25 @@ export type ArticleData = {
 
 const apiURL = "http://localhost:5000/api/article/";
 
-const Article = () => {
+const Article = ({ article }: { article: ArticleData }) => {
   const router = useRouter();
-  const { id } = router.query;
-  const [article, setArticle] = useState<ArticleData | null>();
-  useEffect(() => {
-    const getArticleById = async () => {
-      try {
-        const response = await axios.get(apiURL + id);
-        setArticle(response.data);
-        //console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    if (id) {
-      getArticleById();
-    }
-  }, [id]);
 
   const deleteArticle = async () => {
-    await axios.delete(apiURL + id);
-    setArticle(null);
+    await axios.delete(apiURL + article._id);
     router.push("/");
   };
 
   return (
     <div className="contaniner">
       <div className="sm:items-center m-3 flex flex-col justify-center bg-white border shadow-md rounded-xl p-4 md:p-5">
-        <h1>{article?.title}</h1>
+        <h1>{article.title}</h1>
       </div>
       <div className="sm:items-center m-3 flex flex-col justify-center bg-white border shadow-md rounded-xl p-4 md:p-5">
-        <p>{article?.content}</p>
+        <p>{article.content}</p>
       </div>
       <div className="flex flex-row justify-end p-2 m-2">
-        <img className="avatar-img" src={article?.user_img} alt="u_img" />
-        <h2 className="m-1">{article?.user_name}</h2>
+        <img className="avatar-img" src={article.user_img} alt="u_img" />
+        <h2 className="m-1">{article.user_name}</h2>
       </div>
       <div className="contaniner ">
         <div className="buttom-primary" onClick={deleteArticle}>
@@ -59,7 +41,7 @@ const Article = () => {
         <div
           className="buttom-secondary"
           onClick={() => {
-            router.push("/Edit/" + article?._id);
+            router.push("/Edit/" + article._id);
           }}
         >
           EDIT
@@ -70,3 +52,22 @@ const Article = () => {
 };
 
 export default Article;
+
+export async function getServerSideProps(context: any) {
+  const { id } = context.query;
+
+  try {
+    const response = await axios.get(apiURL + id);
+    const article = response.data;
+
+    return {
+      props: { article },
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      notFound: true,
+    };
+  }
+}
