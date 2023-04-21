4c21fcdece2c42ea0c99fb75568ec11b66db26e0
Nattapong <banker.z412@gmail.com>
2023-04-21 18:54:47 +0700

    learning about SEO and mata tag in next.js

--- frontend/pages/Article/[id].tsx
+++ frontend/pages/Article/[id].tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import React from "react";
-
+import Head from "next/head";
 type ArticleData = {
   _id: string;
   title: string;
@@ -23,31 +23,39 @@ const Article = ({ article }: { article: ArticleData }) => {
   };
 
   return (
-    <div className="contaniner">
-      <div className="sm:items-center m-3 flex flex-col justify-center bg-white border shadow-md rounded-xl p-4 md:p-5">
-        <h1>{article.title}</h1>
-      </div>
-      <div className="sm:items-center m-3 flex flex-col justify-center bg-white border shadow-md rounded-xl p-4 md:p-5 whitespace-pre-wrap overflow-wrap-break-word">
-        {article.content}
-      </div>
-      <div className="flex flex-row justify-end p-2 m-2">
-        <img className="avatar-img" src={article.user_img} alt="u_img" />
-        <h2 className="m-1">{article.user_name}</h2>
-      </div>
-      <div className="flex flex-row justify-end">
-        <div className="buttom-primary w-auto" onClick={deleteArticle}>
-          DEL
+    <>
+      <Head>
+        <title>{article.title}</title>
+        <meta name="author" content={article.user_name}></meta>
+        <meta name="description" content="This is a description of my web page."></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
+      <div className="contaniner">
+        <div className="sm:items-center m-3 flex flex-col  bg-white border shadow-md rounded-xl p-4 md:p-5">
+          <h1>{article.title}</h1>
         </div>
-        <div
-          className="buttom-secondary w-auto"
-          onClick={() => {
-            router.push("/Edit/" + article._id);
-          }}
-        >
-          EDIT
+        <div className=" m-3 flex flex-col  bg-white border shadow-md rounded-xl p-4 md:p-5 whitespace-pre-wrap">
+          <p className="text-left">{article.content}</p>
+        </div>
+        <div className="flex flex-row justify-end p-2 m-2">
+          <img className="avatar-img" src={article.user_img} alt="u_img" />
+          <h2 className="m-1">{article.user_name}</h2>
+        </div>
+        <div className="flex flex-row justify-end">
+          <div className="buttom-primary w-auto" onClick={deleteArticle}>
+            DEL
+          </div>
+          <div
+            className="buttom-secondary w-auto"
+            onClick={() => {
+              router.push("/Edit/" + article._id);
+            }}
+          >
+            EDIT
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
