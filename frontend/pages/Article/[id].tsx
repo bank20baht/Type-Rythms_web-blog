1ebc03fb7b28479a1a0b12173e512da59f32d8d2
Nattapong <banker.z412@gmail.com>
2023-04-25 17:32:19 +0700

    reload when click comment botton will see comment updata [i use reload bc getserversideprop not a setStage comment update is a reload(from my point of view(beginner webdev haha))]

--- frontend/pages/Article/[id].tsx
+++ frontend/pages/Article/[id].tsx
@@ -52,7 +52,8 @@ const Article = ({ article }: { article: ArticleData }) => {
         setCommentData(response.data);
       });
     setCommentData(initalState);
-    router.push("/Article/" + id);
+    //router.push("/Article/" + id);
+    window.location.reload()
   }
 
   return (
