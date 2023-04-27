f237722e65d12635459d4712c85ebf437d3c7e96
Nattapong <banker.z412@gmail.com>
2023-04-27 18:08:13 +0700

    fix user page

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import logo from "@/components/logo.png";
 export default function FourOFour() {
   return <>
     <Head>
@@ -7,6 +8,7 @@ export default function FourOFour() {
 	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8 content-center">
 		<div className="max-w-md text-center">
 			<h2 className="mb-8 font-extrabold text-9xl text-red-400">
+            <img className=""src={logo.src} />
 				<span className="sr-only">Error</span>404
 			</h2>
 			<p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
--- frontend/pages/User/[id].tsx
+++ frontend/pages/User/[id].tsx
@@ -10,8 +10,8 @@ export type ArticleData = {
   user_name: string;
   user_img: string;
   timestamp: string;
-  user_email: string;
-};
+  user_email: string
+}
 
 interface Props {
   articles: ArticleData[];
@@ -21,7 +21,7 @@ interface Props {
 
 export default function User({ articles, currentPage, totalPages }: Props) {
   const router = useRouter();
-  const { id } = router.query;
+  const { id } = router.query
   const goToNextPage = () => {
     window.location.href = `/?page=${currentPage + 1}`;
   };
@@ -44,28 +44,24 @@ export default function User({ articles, currentPage, totalPages }: Props) {
         ></meta>
       </Head>
       <div>
+      <div className="max-w-[120rem] w-full mx-auto sm:items-center m-3 flex flex-col justify-center">
+          <img
+            className="rounded-full w-20 h-20 m-1"
+            src={articles[0].user_img}
+            alt="u_img"
+          />
+          <div className="">{articles[0].user_name}</div>
+          <div>{articles[0].user_email}</div>
+        </div>
         {articles && articles.length > 0 ? (
           articles
             .slice(0)
             .reverse()
             .map((article: any) => {
-              return (
-                <>
-                  <div className="max-w-[120rem] w-full mx-auto sm:items-center m-3 flex flex-col justify-center">
-                    <img
-                      className="rounded-full w-20 h-20 m-1"
-                      src={articles[0].user_img}
-                      alt="u_img"
-                    />
-                    <div className="">{articles[0].user_name}</div>
-                    <div>{articles[0].user_email}</div>
-                  </div>
-                  <CardArticle article={article} key={article._id} />;
-                </>
-              );
+              return <CardArticle article={article} key={article._id} />;
             })
         ) : (
-          <div>No User found.</div>
+          <div>No articles found.</div>
         )}
       </div>
       <div className="flex justify-center">
@@ -90,20 +86,21 @@ export default function User({ articles, currentPage, totalPages }: Props) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  const { id } = query;
+  const { id } = query
   const page = query.page ? Number(query.page) : 1;
   const limit = 5;
-  let articles = null;
-  let totalPages = 0;
+  let articles
+  let totalPages
   try {
     const response = await axios.get(
-      `http://localhost:5000/api/user/${id}?page=${page}&limit=${limit}`
+    `http://localhost:5000/api/user/${id}?page=${page}&limit=${limit}`
     );
     articles = response.data.results;
     totalPages = Math.ceil(response.data.count / limit);
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
+
 
   return { props: { articles, currentPage: page, totalPages } };
 };
