73090aa025b70a7307c5fbc163764a40c3d7c321
Nattapong <banker.z412@gmail.com>
2023-05-05 19:00:22 +0700

    decode JWT in frontendside, I want to make use of secret access token

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -6,16 +6,9 @@ import { decode } from "next-auth/jwt";
 const jwt = require("jsonwebtoken");
 
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
   providers: [
-    // ...add more providers here
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         email: {
           label: "Email",
@@ -63,13 +56,16 @@ export const authOptions: NextAuthOptions = {
                 }
               }
             );
+            const { name, email, image } = decodedAccessToken || { name: "", email: "", image: "" };
+
             return {
-              name: decodedAccessToken?.name,
-              email: decodedAccessToken?.email,
-              image: decodedAccessToken?.image,
+              name,
+              email,
+              image,
               accessToken: accesstoken,
               refreshToken: refreshtoken,
             };
+            
           } else {
             throw new Error("Failed to log in with the given credentials.");
           }
